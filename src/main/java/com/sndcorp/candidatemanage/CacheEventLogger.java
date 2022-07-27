a00b9887450fa5da9431e0bf944e210195065f58
dinesh <dineshvhingwe@gmail.com>
2022-07-27 16:04:37 +0530

    Cached Login

--- src/main/java/com/sndcorp/candidatemanage/CacheEventLogger.java
+++ src/main/java/com/sndcorp/candidatemanage/CacheEventLogger.java
@@ -1,25 +1,18 @@
 package com.sndcorp.candidatemanage;
 
-import java.util.List;
-
 import org.ehcache.event.CacheEvent;
 import org.ehcache.event.CacheEventListener;
-import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-public class CacheEventLogger implements CacheEventListener<SimpleKey, List> {
+public class CacheEventLogger implements CacheEventListener<Object, Object> {
 
 	@Override
-	public void onEvent(CacheEvent<? extends SimpleKey, ? extends List> cacheEvent) {
-		log.debug("Cache Update Notification: Key: {} , Old: {}", cacheEvent.getKey(),
-				cacheEvent.getOldValue());
-		if (null != cacheEvent.getNewValue() && !cacheEvent.getNewValue().isEmpty()) {
-			cacheEvent.getNewValue().forEach((candidate)-> log.debug("new value: {}", candidate));
-		}
+	public void onEvent(CacheEvent<? extends Object, ? extends Object> cacheEvent) {
+		log.debug("Cache Update Notification: Key: {} , Old: {}, New: {}", cacheEvent.getKey(),
+				cacheEvent.getOldValue(), cacheEvent.getNewValue() == null ? " " : cacheEvent.getNewValue() == null);
 	}
-
 }
